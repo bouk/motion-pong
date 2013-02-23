a8d04fdaf5c9ad0418f374d5329b257aa12c8340
Bouke van der Bijl <boukevanderbijl@gmail.com>
2013-02-23 12:16:51 +0100

    Modify Camera Thread to use cv2
    Very slow currently

--- util.py
+++ util.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import cv
+import cv2
 import math
 import numpy
 from operator import itemgetter
@@ -15,20 +16,20 @@ class CameraThread(threading.Thread):
         self.resolution = resolution
         self.circles = list()
         self.scale = scale
-        self.scaled_resolution = map(lambda n: int(n * self.scale), self.resolution)
+        self.scaled_resolution = tuple(map(lambda n: int(n * self.scale), self.resolution))
 
         self.lock = threading.Lock()
 
-        self.camera = cv.CaptureFromCAM(-1)
+        self.camera = cv2.VideoCapture(0)
 
-        cv.SetCaptureProperty(self.camera, cv.CV_CAP_PROP_FRAME_WIDTH, self.resolution[0])
-        cv.SetCaptureProperty(self.camera, cv.CV_CAP_PROP_FRAME_HEIGHT, self.resolution[1])
+        self.camera.set(cv.CV_CAP_PROP_FRAME_WIDTH, self.resolution[0])
+        self.camera.set(cv.CV_CAP_PROP_FRAME_HEIGHT, self.resolution[1])
         self.running = False
 
         self.camera_image = pygame.Surface(self.resolution)
-        self.opencv_image = None
-        self.scaled_opencv_image = cv.CreateImage(self.scaled_resolution, 8, 3)
-        self.gray_opencv_image = cv.CreateImage(cv.GetSize(self.scaled_opencv_image), 8, 1)
+        self.opencv_image = numpy.ndarray((self.resolution[1], self.resolution[0], 3), numpy.uint8)
+        self.scaled_opencv_image = numpy.ndarray((self.scaled_resolution[1], self.scaled_resolution[0], 3), numpy.uint8)
+        self.gray_opencv_image = numpy.ndarray((self.scaled_resolution[1], self.scaled_resolution[0], 1), numpy.uint8)
         self.recalc = 0
 
         self.lower_bound = hsv_to_scalar(130, 0.20, 0.1)
@@ -38,25 +39,24 @@ class CameraThread(threading.Thread):
         self.running = True
 
         while self.running:
-            self.opencv_image = cv.QueryFrame(self.camera)
-            cv.Flip(self.opencv_image, self.opencv_image, 1)
+            result, self.opencv_image = self.camera.read()
+            cv2.flip(src=self.opencv_image, flipCode=1, dst=self.opencv_image)
 
             self.camera_image = cv_to_pygame_surface(self.opencv_image)
 
-            self.recalc = (self.recalc + 1) % 2
+            self.recalc = (self.recalc + 1) % 4
             if self.recalc == 0:
-                cv.Resize(self.opencv_image, self.scaled_opencv_image, cv.CV_INTER_LINEAR)
-                cv.CvtColor(self.scaled_opencv_image, self.scaled_opencv_image, cv.CV_BGR2HSV)
+                cv2.resize(self.opencv_image, self.scaled_resolution, self.scaled_opencv_image)
 
-                cv.InRangeS(self.scaled_opencv_image, self.lower_bound, self.upper_bound, self.gray_opencv_image)
-                cv.Smooth(self.gray_opencv_image, self.gray_opencv_image, cv.CV_GAUSSIAN, 9, 9)
-                cv.Canny(self.gray_opencv_image, self.gray_opencv_image, 50, 200, 3)
-                storage = cv.CreateMat(self.scaled_opencv_image.width, 1, cv.CV_32FC3)
+                cv2.cvtColor(src=self.scaled_opencv_image, dst=self.scaled_opencv_image, code=cv.CV_BGR2HSV)
 
-                cv.HoughCircles(self.gray_opencv_image, storage, cv.CV_HOUGH_GRADIENT, 2, self.gray_opencv_image.width/2, 200, 100)
+                cv2.inRange(self.scaled_opencv_image, self.lower_bound, self.upper_bound, self.gray_opencv_image)
+                cv2.GaussianBlur(src=self.gray_opencv_image, ksize=(3, 3), sigmaX=9, dst=self.gray_opencv_image)
+                cv2.Canny(self.gray_opencv_image, 50, 200, self.gray_opencv_image, 3)
+                storage = cv2.HoughCircles(image=self.gray_opencv_image, method=cv.CV_HOUGH_GRADIENT, dp=2, minDist=self.gray_opencv_image.shape[0] / 2, param1=200, param2=100)
 
-                if storage.rows > 0:
-                    self.circles = sorted(map(lambda a: (int(a[0][0] / self.scale), int(a[0][1] / self.scale), int(a[0][2] / self.scale)), numpy.asarray(storage)), key=itemgetter(2), reverse=True)
+                if storage != None and len(storage[0]) > 0:
+                    self.circles = sorted(map(lambda a: (int(a[0] / self.scale), int(a[1] / self.scale), int(a[2] / self.scale)), storage[0]), key=itemgetter(2), reverse=True)
                 else:
                     self.circles = list()
 
@@ -80,9 +80,10 @@ def rect_intersect(rect_a, rect_b):
     return False
 
 def cv_to_pygame_surface(img):
-    img_rgb = cv.CreateMat(img.height, img.width, cv.CV_8UC3)
-    cv.CvtColor(img, img_rgb, cv.CV_BGR2RGB)
-    return pygame.image.frombuffer(img_rgb.tostring(), cv.GetSize(img_rgb), 'RGB')
+    img_rgb = numpy.ndarray(img.shape, img.dtype)
+    cv2.cvtColor(src=img, dst=img_rgb, code=cv.CV_BGR2RGB)
+
+    return pygame.image.frombuffer(img_rgb.tostring(), (img.shape[1], img.shape[0]), 'RGB')
 
 def rad_to_deg(rad):
     return rad / math.pi * 180.0
